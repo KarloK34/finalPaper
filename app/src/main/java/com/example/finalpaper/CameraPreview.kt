cf895ef321234f86ef24375341dc47751bad0aa7
Karlo Kraml <karlokraml2@gmail.com>
2024-04-17 13:54:08 +0200

    pinch-to-zoom gesture

--- app/src/main/java/com/example/finalpaper/CameraPreview.kt
+++ app/src/main/java/com/example/finalpaper/CameraPreview.kt
@@ -2,8 +2,15 @@ package com.example.finalpaper
 
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 
@@ -13,13 +20,27 @@ fun CameraPreview(
     modifier: Modifier = Modifier
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
-    AndroidView(
-        factory = {
-            PreviewView(it).apply {
-                this.controller = controller
-                controller.bindToLifecycle(lifecycleOwner)
-            }
-        },
+    val zoomState = remember { mutableFloatStateOf(1f) }
+    BoxWithConstraints(
         modifier = modifier
-    )
+    ) {
+        val state = rememberTransformableState { zoom, _ , _ ->
+            zoomState.floatValue *= zoom
+        }
+        AndroidView(
+            factory = { context ->
+                PreviewView(context).apply {
+                    this.controller = controller
+                    controller.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer {
+                    scaleX = zoomState.floatValue
+                    scaleY = zoomState.floatValue
+                }
+                .transformable(state, enabled = false)
+        )
+    }
 }
